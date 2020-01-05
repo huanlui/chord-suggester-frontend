44b0771d781a52fc9e2697b28acb6cfda0fa1b40
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-01-05 17:28:26 +0100

    Custom chord prototype improved

--- src/components/SuggestedChordSelector.js
+++ src/components/SuggestedChordSelector.js
@@ -1,8 +1,12 @@
-import React, {useEffect} from 'react';
+import React , { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import * as Vex from 'vexflow'
+import { Typography, Tooltip, IconButton, Fab } from '@material-ui/core'
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,7 +24,7 @@ const drawChord2 = ()  => {
   const VF = Vex.Flow;
 
 var vf = new VF.Factory({
-  renderer: {elementId: 'foo', width: 250, height: 150}
+  renderer: {elementId: 'foo', width: 90, height: 110}
 });
 
 var score = vf.EasyScore();
@@ -41,16 +45,16 @@ var div = document.getElementById("foo")
 var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Size our svg:
-renderer.resize(500, 500);
+renderer.resize(90,80);
 
 // And get a drawing context:
 var context = renderer.getContext();
 
 // Create a stave at position 10, 40 of width 400 on the canvas.
-var stave = new VF.Stave(60, 0, 120);
+var stave = new VF.Stave(0, -20, 80);
 
 // Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
+stave.addClef("treble");//.addTimeSignature("4/4");
 
 // Connect it to the rendering context and draw!
 stave.setContext(context).draw();
@@ -59,7 +63,7 @@ stave.setContext(context).draw();
 var notes = [
   // new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "w" }).addModifier(0, new Vex.Flow.Annotation("C")
   // .setVerticalJustification(Vex.Flow.Annotation.VerticalJustify.TOP))
-  new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "w" })
+  new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "a/4"], duration: "w" })
 ];
 
 // Create a voice in 4/4 and add above notes
@@ -84,8 +88,21 @@ const SuggestedChordSelector = () => {
       <React.Fragment>
         <Grid item xs={4}>
           <Paper className={classes.paper}>
-              C
-              <div id='foo' style={{width: 100, height:100}}></div>
+              <Typography variant="h6" color='primary'>C#maj</Typography>
+              <div style={{textAlign:'center'}}>
+                <div id='foo' style={{width: 100, display:'inline-block'}}></div>
+              </div>
+              <Typography>Probability: 0.5%</Typography>
+              <Tooltip title="Listen">
+                <IconButton aria-label="Listen">
+                  <PlayCircleOutlineIcon fontSize='large' color='primary'></PlayCircleOutlineIcon>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Add">
+                <IconButton aria-label="Add">
+                  <AddBoxIcon fontSize='large' color='primary'></AddBoxIcon>
+                </IconButton>
+              </Tooltip>
          </Paper>
         </Grid>
         <Grid item xs={4}>
@@ -100,6 +117,7 @@ const SuggestedChordSelector = () => {
 
   return (
     <div className={classes.root}>
+      <Typography variant="h5" color='secondary'>Suggested chords</Typography>
       <Grid container spacing={1}>
         <Grid container item xs={12} spacing={3}>
           <FormRow />
