7809dba67ef82b12b08d71c15fffc563407c1ed0
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-01-06 02:27:52 +0100

    clear button

--- src/components/SheetOperations.js
+++ src/components/SheetOperations.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tooltip, IconButton } from '@material-ui/core'
 import BackspaceIcon from '@material-ui/icons/Backspace';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { playChords } from '../utils/Player';
 
 const SheetOperations = () => {
@@ -14,6 +15,11 @@ return (<div>
               <Tooltip title="Remove last chord">
                 <IconButton aria-label="Remove last chord">
                   <BackspaceIcon fontSize='large' color='primary'></BackspaceIcon>
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Clear composition">
+                <IconButton aria-label="Clear composition">
+                  <DeleteForeverIcon fontSize='large' color='primary'></DeleteForeverIcon>
                 </IconButton>
               </Tooltip>
         </div>)
