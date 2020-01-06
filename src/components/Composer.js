4898d3f056de6d6db2cd0381197b7310b3c159c4
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-01-06 22:52:48 +0100

    Make SheetActions work

--- src/components/Composer.js
+++ src/components/Composer.js
@@ -3,20 +3,26 @@ import React, { useState, useEffect } from 'react';
 import Sheet from './Sheet';
 import SheetActions from './SheetActions';
 import Chord from '../utils/Chord';
+import { playChords } from '../utils/Player';
 
 const Composer = ({suggestedChords,onChordsModified}) => {
     const initialChords = ['C', 'D'].map(chordName => new Chord(chordName));
-    const [chords] = useState(initialChords);
+    const [chords, setChords] = useState(initialChords);
 
     useEffect( () =>
     {
       onChordsModified(chords);
     }, [chords]);
 
+
     return (
         <>
           <Sheet chords={chords}></Sheet>
-          <SheetActions></SheetActions>
+          <SheetActions 
+            play={() => playChords(chords)}
+            removeLast={() => setChords(previousChords => previousChords.slice(0, -1) )}
+            clear={() => setChords([])}
+          ></SheetActions>
           <ChordSelector></ChordSelector>
         </>
     )
--- src/components/SheetActions.js
+++ src/components/SheetActions.js
@@ -3,28 +3,27 @@ import { Tooltip, IconButton } from '@material-ui/core'
 import BackspaceIcon from '@material-ui/icons/Backspace';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import { playChords } from '../utils/Player';
-import Chord from '../utils/Chord';
 
-const SheetActions = () => {
-  const chords = ['C', 'A'].map(chordName => new Chord(chordName));
-return (<div>
-              <Tooltip title="Listen" placement="top" arrow>
-                <IconButton aria-label="Listen" onClick={() => playChords(chords)}>
-                  <PlayCircleOutlineIcon fontSize='large' color='primary'></PlayCircleOutlineIcon>
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Remove last chord" placement="top" arrow>
-                <IconButton aria-label="Remove last chord">
-                  <BackspaceIcon fontSize='large' color='primary'></BackspaceIcon>
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Clear composition" placement="top" arrow>
-                <IconButton aria-label="Clear composition">
-                  <DeleteForeverIcon fontSize='large' color='primary'></DeleteForeverIcon>
-                </IconButton>
-              </Tooltip>
-        </div>)
+const SheetActions = ({play, removeLast, clear}) => {
+return (
+    <div>
+        <Tooltip title="Listen" placement="top" arrow>
+          <IconButton aria-label="Listen" onClick={play}>
+            <PlayCircleOutlineIcon fontSize='large' color='primary'></PlayCircleOutlineIcon>
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Remove last chord" placement="top" arrow>
+          <IconButton aria-label="Remove last chord" onClick={removeLast}>
+            <BackspaceIcon fontSize='large' color='primary'></BackspaceIcon>
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Clear composition" placement="top" arrow>
+          <IconButton aria-label="Clear composition" onClick={clear}>
+            <DeleteForeverIcon fontSize='large' color='primary'></DeleteForeverIcon>
+          </IconButton>
+        </Tooltip>
+    </div>
+  )
 }
 
 export default SheetActions;
