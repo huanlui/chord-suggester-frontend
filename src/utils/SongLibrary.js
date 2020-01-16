ac231a268b7d7456f228272bb59bfd03a701c5a4
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-01-16 21:24:43 +0100

    Add new songs

--- src/utils/SongLibrary.js
+++ src/utils/SongLibrary.js
@@ -13,3 +13,22 @@ export const MajorIIVVI = ['F', 'C', 'F','Bb', 'C', 'F'].map(chordName => new Ch
 //http://blogs.bu.edu/sjbon/files/2015/03/Bach-Prelude-BWV-846-Condensed.pdf
 export const Bach = ['C', 'Dm', 'G7','C', 'Am', 'D7', 'G', 'C', 'Am', 'D7', 'G', 'Dbm7b5', 'Dm', 'Fdim', 'C', 'F', 'D', 'G7', 'C'].map(chordName => new Chord(chordName));
 
+export const Wonderwall = ['Em' ,  'G'  , 'D'  , 'A7sus4', 
+                            'Em' ,  'G' ,  'D'  , 'A7sus4',
+                             'C' , 'D'  ,'A7sus4'].map(chordName => new Chord(chordName));
+
+export const Clockwise = ['C',
+'G',
+'D',
+'A',
+'E',
+'B',
+'F#',
+'Db'].map(chordName => new Chord(chordName));
+
+export const CounterClockwise = ['C',
+'F',
+'Bb',
+'Eb',
+'Ab',
+'Db'].map(chordName => new Chord(chordName));
