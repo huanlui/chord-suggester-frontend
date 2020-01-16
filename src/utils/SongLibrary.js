94e5a43bdf2884090f43a842f00bfa10ab901466
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2020-01-16 01:10:22 +0100

    New songs

--- src/utils/SongLibrary.js
+++ src/utils/SongLibrary.js
@@ -5,3 +5,11 @@ export const Pachebel = ['D' ,'A' , 'Bm' , 'F#m' , 'G' , 'D'  ,'G',  'A'].map(ch
 export const MajorIVI = ['F', 'C', 'F','C', 'F'].map(chordName => new Chord(chordName));
 
 export const MinorIVI = ['Fm', 'C', 'Fm','C', 'Fm'].map(chordName => new Chord(chordName));
+
+export const NeutralIVI = ['F5', 'C5', 'F5','C5', 'F5'].map(chordName => new Chord(chordName));
+
+export const MajorIIVVI = ['F', 'C', 'F','Bb', 'C', 'F'].map(chordName => new Chord(chordName));
+
+//http://blogs.bu.edu/sjbon/files/2015/03/Bach-Prelude-BWV-846-Condensed.pdf
+export const Bach = ['C', 'Dm', 'G7','C', 'Am', 'D7', 'G', 'C', 'Am', 'D7', 'G', 'Dbm7b5', 'Dm', 'Fdim', 'C', 'F', 'D', 'G7', 'C'].map(chordName => new Chord(chordName));
+
